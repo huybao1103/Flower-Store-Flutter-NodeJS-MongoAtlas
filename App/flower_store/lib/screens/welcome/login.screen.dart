b51eae1a51541c3c2936947878046545eb95bfe5
NhatAnhHuynh <114375962+anhnhatcc700@users.noreply.github.com>
2024-06-27 13:33:40 +0700

    LoginScreen

--- App/flower_store/lib/screens/welcome/login.screen.dart
+++ App/flower_store/lib/screens/welcome/login.screen.dart
@@ -3,94 +3,149 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
+
+import '../../shared/components/input_decoration.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
-  _LoginScreenState createState() => _LoginScreenState();
+  State<LoginScreen> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
   final _loginForm = GlobalKey<FormBuilderState>();
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: gradientBackground
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(top: 100),
-            ),
-            const SizedBox(
-              width: double.infinity,
-              child: Text(
-                'Đăng Nhập',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+      resizeToAvoidBottomInset: false,
+      body: new GestureDetector(
+        onTap: () {
+         FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: gradientBackground,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(20, 30, 20, 0),
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/images/logo_shop.png',
+                  width: 200,
                 ),
-                textAlign: TextAlign.center,
-              ),
+                const Text(
+                  "Login",
+                  style: TextStyle(
+                      fontSize: 28,
+                      color: Color.fromARGB(255, 9, 9, 9),
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                FormBuilder(
+                      key: _loginForm,
+                      child: Column(
+                        children: getLoginForm()
+                      )
+                    ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  const Text(
+                    "Need a new account ?",
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Sign up",
+                      style: TextStyle(color: Color(0xff920000)),
+                    ),
+                  ),
+                ]),
+              ],
             ),
-            const SizedBox(
-              height: 40,
-            ),
-            FormBuilder(
-                key: _loginForm,
-                child: Column(
-                  children: [
-                    FormBuilderTextField(
-                      name: 'email',
-                      validator: FormBuilderValidators.compose([
-                        FormBuilderValidators.required(),
-                        FormBuilderValidators.email(),
-                      ]),
-                      decoration: InputDecoration(
-                        label: Title(
-                        color: Colors.grey, child: const Text('Email')),
-                        border: const OutlineInputBorder()
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    FormBuilderTextField(
-                      name: 'password',
-                      validator: FormBuilderValidators.compose([
-                        FormBuilderValidators.required(),
-                      ]),
-                      decoration: const InputDecoration(
-                          labelText: 'Password', border: OutlineInputBorder()),
-                    ),
-                    MaterialButton(
-                      color: Theme.of(context).colorScheme.secondary,
-                      onPressed: () {
-                        // Validate and save the form values
-                        _loginForm.currentState?.saveAndValidate();
-                        debugPrint(_loginForm.currentState?.value.toString());
-
-                        // On another side, can access all field values without saving form with instantValues
-                        _loginForm.currentState?.validate();
-                        debugPrint(
-                            _loginForm.currentState?.instantValue.toString());
-                      },
-                      child: const Text('Login'),
-                    )
-                  ],
-                )),
-          ],
+          ),
         ),
-        )
       ),
     );
   }
+
+  List<Widget> getLoginForm() {
+    return [
+      genericFieldContainer(
+        field: FormBuilderTextField(
+          name: 'email',
+          validator: FormBuilderValidators.compose([
+            FormBuilderValidators.required(),
+            FormBuilderValidators.email(),
+          ]),
+          decoration: genericInputDecoration(label: 'Email')
+        )
+      ),
+      genericFieldContainer(
+        field: FormBuilderTextField(
+          name: 'password',
+          validator: FormBuilderValidators.compose([
+            FormBuilderValidators.required(),
+          ]),
+          obscureText: true,
+          enableSuggestions: false,
+          autocorrect: false,
+          decoration: genericInputDecoration(label: 'Password'),
+        ),
+      ),
+      const SizedBox(height: 10),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                alignment: Alignment.centerRight,
+                child: SizedBox(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Forgot password',
+                      style: TextStyle(color: Color(0xff920000)),
+                      textAlign: TextAlign.right,
+                    ),
+                  ),
+                ),
+              ),
+      const SizedBox(height: 10),
+      MaterialButton(
+        color: const Color(0xFFFFEED0),
+        minWidth: double.infinity,
+        padding: const EdgeInsets.all(15),
+        shape: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: const BorderSide(color: Colors.transparent)
+        ),
+        elevation: 10,
+        focusElevation: 5,
+        onPressed: () {
+          // Validate and save the form values
+          _loginForm.currentState?.saveAndValidate();
+          debugPrint(_loginForm.currentState?.value.toString());
+        },
+        child: const Text(
+          'Login',
+          style: TextStyle(
+            fontSize: 18,
+          ),
+        ),
+      ),
+    ];
+  }
 }
+
