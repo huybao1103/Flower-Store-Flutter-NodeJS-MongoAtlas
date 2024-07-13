8a8d8138041458901bffdfe0d2f7161183445942
huybao1103 <l.q.h.bao@gmail.com>
2024-07-13 16:12:16 +0700

    Update app.js

--- Api/app.js
+++ Api/app.js
@@ -5,7 +5,7 @@ const app = express();
 module.exports = {app};
 
 const { ServerApiVersion } = require('mongodb');
-const credentials = '/Users/thodo/Documents/Coding/HUFLIT/MobileNangCao/LT/FlowerStore/Flower-Store-Flutter-NodeJS-MongoAtlas/Api/Certificate/X509-cert-7353008439470924153.pem'
+const credentials = 'Api/Certificate/X509-cert-7353008439470924153.pem'
 
 const client = mongoose.connect('mongodb+srv://testdb.tpyzpae.mongodb.net/FlowerDB?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&appName=TestDB', {
   tlsCertificateKeyFile: credentials,
